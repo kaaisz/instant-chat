dfbf0a5209bd9dd5e62a8930d1cedd982d1c87d2
Kaai Suzuki <suzydp@me.com>
2018-12-13 14:07:59 -0800

    Add Avatar

--- src/Components/ChatList.jsx
+++ src/Components/ChatList.jsx
@@ -1,9 +1,11 @@
 // Messagelist.js
 import React from 'react';
 import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core';
+import AvatarImg from './../assets/img/bio_pict.jpg';
 
 
 const styles = theme => ({
@@ -15,6 +17,9 @@ const styles = theme => ({
     textAlign: 'center',
     color: theme.palette.text.primary,
   },
+  avatar: {
+    margin: 'auto',
+  }
 })
 
 class ChatList extends React.Component {
@@ -25,7 +30,7 @@ class ChatList extends React.Component {
         <Grid container spacing={24}>
           <Grid item xs={12}>
             <Paper className={classes.paper}>
-              <p>xs=12</p>
+              <Avatar alt="Katie Strathcona" src={ AvatarImg } className={classes.avatar}/>
               <p>this is ChatList Component</p>
               {/* 親から渡ってきた中身が表示 */}
               <p>{this.props.message}</p>
